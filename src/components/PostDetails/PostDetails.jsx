7563bbb20a7e99c7a9e2419715eb306393523a29
rootnure <nur.diu.2791@gmail.com>
2023-09-19 18:52:24 +0600

    Get Dynamic Route dependency using useParams()

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
 
@@ -6,16 +6,19 @@ const PostDetails = () => {
 
     const navigate = useNavigate();
 
-    const { id, title, body } = post;
-
+    const {postId} = useParams();
+    console.log(postId);
+    
     const detailsPostStyles = {
         display: 'grid',
         gridTemplateColumns: 'repeat(4, 1fr)'
     }
-
+    
     const handleGoBack = () => {
         navigate(-1);
     }
+
+    const { id, title, body } = post;
 
     return (
         <div style={detailsPostStyles}>
