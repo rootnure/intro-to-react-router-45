dc4e17ed434f059c01188d97828a62b17cf2ccab
rootnure <nur.diu.2791@gmail.com>
2023-09-19 19:44:53 +0600

    Active Route, useNavigation() hook, useLocation() hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './Header.css'
 
 const Header = () => {
@@ -8,12 +8,12 @@ const Header = () => {
                 <nav>
                     <span>My Website</span>
                     <span>
-                        <Link to="/">Home</Link>
-                        <Link to="contact">Contact</Link>
-                        <Link to="posts">Posts</Link>
-                        <Link to="about">About Us</Link>
-                        <Link to="users">Users</Link>
-                        <Link to="app">App</Link>
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="users">Users</NavLink>
+                        <NavLink to="posts">Posts</NavLink>
+                        <NavLink to="app">App</NavLink>
+                        <NavLink to="contact">Contact</NavLink>
+                        <NavLink to="about">About Us</NavLink>
                     </span>
                 </nav>
             </header>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 
 const Home = () => {
+
+    const navigation = useNavigation();
+
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ?
+                <p>Loading...</p> :
+                <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     );
